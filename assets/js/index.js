889fac44c32a93a8ae4a24295709db9bcbdd0b44
Long Phap Long Phap <longphappy@gmail.com>
2022-01-19 18:06:40 +0700

    fix bug submenu mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ const modal = document.querySelector(".js-modal");
 const modalContainer = document.querySelector(".js-modal-container");
 const closeModal = document.querySelector(".js-modal-close");
 
+// Modal
 buyBtns.forEach((buyBtn) => {
   buyBtn.addEventListener("click", () => {
     modal.classList.add("open");
@@ -19,4 +20,34 @@ modal.addEventListener("click", () => {
 
 modalContainer.addEventListener("click", (event) => {
   event.stopPropagation();
+});
+
+// Mobile
+const header = document.getElementById("header");
+const mobileMenu = document.getElementById("mobile-menu");
+const headerHeight = header.clientHeight;
+const menuItems = document.querySelectorAll("#nav li a[href*='#']");
+
+// Đóng mở menu mobile
+mobileMenu.addEventListener("click", () => {
+  if (header.clientHeight === headerHeight) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = null;
+  }
+});
+
+// Tự động đóng menu khi select
+menuItems.forEach((item) => {
+  var isParentMenu =
+    item.nextElementSibling &&
+    item.nextElementSibling.classList.contains("subnav");
+
+  item.addEventListener("click", (e) => {
+    if (isParentMenu) {
+      e.preventDefault();
+    } else {
+      header.style.height = null;
+    }
+  });
 });
